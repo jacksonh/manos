7d61f47c80a87d008c7bc18ce354ba96556d6f51
Jackson Harper <jackson@novell.com>
2010-02-17 21:39:32 -0500

    Get rid of some extra files.

--- template/minge/tokenizer.cs
+++ template/minge/tokenizer.cs
@@ -3,10 +3,52 @@ using System;
 using System.IO;
 using System.Text;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
 
 namespace Mango.Templates.Minge {
+
+	public class Token {
+
+		public Token (int line, int col, TokenType type, string value)
+		{
+			Line = line;
+			Column = col;
+			Type = type;
+			Value = value;
+		}
+
+		public Token (int line, int col, TokenType type, string value, object tok_value) : this (line, col, type, value)
+		{
+			TokenizedValue = tok_value;
+		}
+
+		public int Line {
+			get;
+			private set;
+		}
+
+		public int Column {
+			get;
+			private set;
+		}
+
+		public TokenType Type {
+			get;
+			private set;
+		}
+
+		public string Value {
+			get;
+			private set;
+		}
+
+		public object TokenizedValue {
+			get;
+			private set;
+		}
+	}
 
 	public class MingeTokenizer {
 
@@ -215,6 +257,110 @@ namespace Mango.Templates.Minge {
 			PutbackChar (c);
 
 			return number;
+		}
+	}
+
+	
+	public enum TokenType {
+		TOKEN_ADD,
+		TOKEN_ASSIGN,
+		TOKEN_COLON,
+    		TOKEN_COMMA,
+    		TOKEN_DIV,
+		TOKEN_DOT,
+		TOKEN_DOUBLE,
+		TOKEN_EQ,
+		TOKEN_FLOORDIV,
+		TOKEN_GT,
+		TOKEN_GTEQ,
+		TOKEN_LBRACE,
+		TOKEN_LBRACKET,
+		TOKEN_LPAREN,
+		TOKEN_LT,
+		TOKEN_LTEQ,
+		TOKEN_MOD,
+		TOKEN_MUL,
+		TOKEN_NE,
+		TOKEN_PIPE,
+		TOKEN_POW,
+		TOKEN_RBRACE,
+		TOKEN_RBRACKET,
+		TOKEN_RPAREN,
+		TOKEN_SEMICOLON,
+		TOKEN_SUB,
+		TOKEN_TILDE,
+		TOKEN_WHITESPACE,
+		TOKEN_INTEGER,
+		TOKEN_NAME,
+		TOKEN_STRING,
+		TOKEN_QUOTED_STRING,
+		TOKEN_OPERATOR,
+		TOKEN_BLOCK_BEGIN,
+		TOKEN_BLOCK_END,
+		TOKEN_VARIABLE_BEGIN,
+		TOKEN_VARIABLE_END,
+		TOKEN_RAW_BEGIN,
+		TOKEN_RAW_END,
+		TOKEN_COMMENT_BEGIN,
+		TOKEN_COMMENT_END,
+		TOKEN_COMMENT,
+		TOKEN_DATA,
+		TOKEN_INITIAL,
+		TOKEN_EOF,
+    	}
+
+	
+	public class TokenOperators {
+
+		private Dictionary<int,TokenType> single_char_ops = new Dictionary<int,TokenType> ();
+		private Dictionary<string,TokenType> double_char_ops = new Dictionary<string,TokenType> ();
+
+		public TokenOperators (Environment env)
+		{
+			single_char_ops.Add ('+', TokenType.TOKEN_ADD);
+			single_char_ops.Add ('-', TokenType.TOKEN_SUB);
+			single_char_ops.Add ('/', TokenType.TOKEN_DIV);
+			single_char_ops.Add ('*', TokenType.TOKEN_MUL);
+			single_char_ops.Add ('%', TokenType.TOKEN_MOD);
+			single_char_ops.Add ('~', TokenType.TOKEN_TILDE);
+			single_char_ops.Add ('[', TokenType.TOKEN_LBRACKET);
+			single_char_ops.Add (']', TokenType.TOKEN_RBRACKET);
+			single_char_ops.Add ('(', TokenType.TOKEN_LPAREN);
+			single_char_ops.Add (')', TokenType.TOKEN_RPAREN);
+			single_char_ops.Add ('{', TokenType.TOKEN_LBRACE);
+			single_char_ops.Add ('}', TokenType.TOKEN_RBRACE);
+			single_char_ops.Add ('>', TokenType.TOKEN_GT);
+			single_char_ops.Add ('<', TokenType.TOKEN_LT);
+			
+			single_char_ops.Add ('=', TokenType.TOKEN_ASSIGN);
+			single_char_ops.Add ('.', TokenType.TOKEN_DOT);
+			single_char_ops.Add (':', TokenType.TOKEN_COLON);
+			single_char_ops.Add ('|', TokenType.TOKEN_PIPE);
+			single_char_ops.Add (',', TokenType.TOKEN_COMMA);
+			single_char_ops.Add (';', TokenType.TOKEN_SEMICOLON);
+
+			double_char_ops.Add ("//", TokenType.TOKEN_FLOORDIV);
+			double_char_ops.Add ("**", TokenType.TOKEN_POW);
+			double_char_ops.Add ("==", TokenType.TOKEN_EQ);
+			double_char_ops.Add ("!=", TokenType.TOKEN_NE);
+			double_char_ops.Add (">=", TokenType.TOKEN_GTEQ);
+			double_char_ops.Add ("<=", TokenType.TOKEN_LTEQ);
+
+			// These should be environment dependendant
+			double_char_ops.Add (env.BlockStartString, TokenType.TOKEN_BLOCK_BEGIN);
+			double_char_ops.Add (env.BlockEndString, TokenType.TOKEN_BLOCK_END);
+			double_char_ops.Add (env.VariableStartString, TokenType.TOKEN_VARIABLE_BEGIN);
+			double_char_ops.Add (env.VariableEndString, TokenType.TOKEN_VARIABLE_END);
+			double_char_ops.Add (env.CommentStartString, TokenType.TOKEN_COMMENT_BEGIN);
+			double_char_ops.Add (env.CommentEndString, TokenType.TOKEN_COMMENT_END);
+		}
+
+		public Dictionary<int,TokenType> SingleCharOps {
+			get { return single_char_ops; }
+		}
+
+		public Dictionary<string,TokenType> DoubleCharOps {
+			get { return double_char_ops; }
 		}
 	}
 }
