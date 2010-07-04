e245f4a624de3dd92ad34ede1470e982eb060ea9
Jackson Harper <jackson@novell.com>
2010-07-03 23:18:26 -0400

    Flesh out the request a little more.  Add query string parsing.

--- server/Mango.Server/HttpRequest.cs
+++ server/Mango.Server/HttpRequest.cs
@@ -8,6 +8,8 @@ using System.Net;
 using System.Net.Sockets;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+
 
 using Mono.Unix.Native;
 
@@ -16,13 +18,15 @@ namespace Mango.Server {
 
 	public class HttpRequest {
 
-		public HttpRequest (HttpConnection connection, HttpHeaders headers, string verb, string path, bool support_1_1)
+		public HttpRequest (HttpConnection connection, HttpHeaders headers, string method, string resource, bool support_1_1)
 		{
 			Connection = connection;
 			Headers = headers;
-			Verb = verb;
-			Path = path;
+			Method = method;
+			ResourceUri = resource;
 			Http_1_1_Supported = support_1_1;
+
+			SetPathAndQuery ();
 		}
 
 		public HttpConnection Connection {
@@ -35,12 +39,12 @@ namespace Mango.Server {
 			private set;
 		}
 
-		public string Verb {
+		public string Method {
 			get;
 			private set;
 		}
 
-		public string Path {
+		public string ResourceUri {
 			get;
 			private set;
 		}
@@ -48,6 +52,55 @@ namespace Mango.Server {
 		public bool Http_1_1_Supported {
 			get;
 			private set;
+		}
+
+		public string LocalPath {
+			get;
+			private set;
+		}
+		
+		public NameValueCollection PostData {
+			get;
+			private set;
+		}
+
+		public NameValueCollection QueryData {
+			get;
+			private set;
+		}
+
+		private void SetPathAndQuery ()
+		{
+			// This is used with the OPTIONS verb
+			if (ResourceUri == "*")
+				return;
+
+			string uri = ResourceUri;
+			if (!uri.StartsWith ("http://"))
+				uri = "http://host.com" + uri;
+
+			Uri u;
+			if (!Uri.TryCreate (uri, UriKind.Absolute, out u)) {
+				Connection.Abort (400, "Invalid resource path. '{0}'", ResourceUri);
+				return;
+			}
+
+			LocalPath = u.LocalPath;
+			QueryData = HttpUtility.ParseQueryString (u.Query);
+		}
+
+		internal void SetWwwFormData (byte [] data)
+		{
+			string post = Encoding.ASCII.GetString (data);
+
+			Console.WriteLine ("POST:  {0}", post);
+		}
+
+		internal void SetMultiPartFormData (byte [] data)
+		{
+			string post = Encoding.ASCII.GetString (data);
+
+			Console.WriteLine ("POST:  {0}", post);
 		}
 	}
 }
