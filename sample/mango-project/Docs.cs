b9dab535a695fcfa87f7d2a4aae61546b038cf74
Jackson Harper <jackson@novell.com>
2010-07-10 00:07:53 -0400

    Add a docs module to the sample project.

---
+++ sample/mango-project/Docs.cs
@@ -0,0 +1,18 @@
+
+using Mango;
+using Mango.Templates.Minge;
+
+namespace MangoProject {
+
+	public class Docs : MangoModule {
+
+		public static void GettingStarted (MangoContext context)
+		{
+		}
+
+		public static void Api (MangoContext context)
+		{
+		}
+	}
+}
+
