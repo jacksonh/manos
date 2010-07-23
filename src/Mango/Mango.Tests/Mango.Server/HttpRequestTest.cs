248f7d79569847952abb75ad967c54365e887a4e
Jackson Harper <jackson@novell.com>
2010-07-23 01:29:22 -0400

    Add tests directory for Mango.Server

---
+++ src/Mango/Mango.Tests/Mango.Server/HttpRequestTest.cs
@@ -0,0 +1,24 @@
+
+using System;
+using NUnit.Framework;
+
+using Mango;
+using Mango.Server;
+
+namespace Mango.Tests
+{
+
+
+	[TestFixture()]
+	public class HttpRequestTest
+	{
+
+		[Test()]
+		public void TestNoNullQueryData ()
+		{
+			// var r = new HttpRequest (new MockHttpTransaction (), .... );
+			
+			// Assert.NotNull (r.QueryData);
+		}
+	}
+}
