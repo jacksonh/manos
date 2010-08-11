ee73c5fff887f1b2b65bf47d3a384f4219187f28
Jackson Harper <jackson@novell.com>
2010-08-10 23:50:52 -0400

    Add some cool stuff from html5boilerplate.com for the default project layout.

---
+++ data/mango-tool/layouts/default/Content/js/plugins.js
@@ -0,0 +1,43 @@
+
+// remap jQuery to $
+(function($){
+
+ 
+
+
+
+
+
+ 
+
+
+
+})(window.jQuery);
+
+
+
+// usage: log('inside coolFunc',this,arguments);
+// paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console){
+    console.log( Array.prototype.slice.call(arguments) );
+  }
+};
+
+
+
+// catch all document.write() calls
+(function(){
+  var docwrite = document.write;
+  document.write = function(q){ 
+    log('document.write(): ',q); 
+    if (/docwriteregexwhitelist/.test(q)) docwrite(q);  
+  }
+})();
+
+
+// background image cache bug for ie6. www.mister-pixel.com/#Content__state=
+/*@cc_on   @if (@_win32) { document.execCommand("BackgroundImageCache",false,true) }   @end @*/
+
