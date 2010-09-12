52038d474aca08ee601f628c209dba0120d59e87
Jackson Harper <jacksonh@gmail.com>
2010-09-12 00:17:50 -0400

    Partial implementation of the RequestDataDictionary.
    	This guy is a 'cascading' dictionary, that will takes several
    	NameValueCollections (UrlData, RequestData, ect) and 'merge'
    	them into a single dictionary.

---
+++ src/Manos.Tests/Manos.Server/RequestDataDictionaryTest.cs
@@ -0,0 +1,314 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+using NUnit.Framework;
+
+namespace Manos.Server.Tests
+{
+	[TestFixture()]
+	public class RequestDataDictionaryTest
+	{
+		[Test()]
+		public void Count_NoItems_ReturnsZero ()
+		{
+			var rdd = new RequestDataDictionary ();
+			
+			Assert.AreEqual (0, rdd.Count);
+		}
+		
+		[Test]
+		public void Count_SingleItem_ReturnsOne ()
+		{
+			var rdd = new RequestDataDictionary ();
+			
+			rdd.Add ("foobar", "baz");
+			
+			Assert.AreEqual (1, rdd.Count);
+		}
+		
+		[Test]
+		public void Count_SingleItemInCollection_ReturnsOne ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			Assert.AreEqual (1, rdd.Count);
+		}
+		
+		[Test]
+		public void Count_ItemInCollectionAndItemAdded_ReturnsTwo ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			rdd.Add ("foobar", "baz");
+			
+			Assert.AreEqual (2, rdd.Count);
+		}
+		
+		[Test]
+		public void Clear_SingleItemAdded_SetsCountZero ()
+		{
+			var rdd = new RequestDataDictionary ();
+
+			rdd.Add ("foobar", "baz");
+			rdd.Clear ();
+			
+			Assert.AreEqual (0, rdd.Count);
+		}
+		
+		[Test]
+		public void Clear_ItemInCollection_SetsCountZero ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			rdd.Clear ();
+			Assert.AreEqual (0, rdd.Count);
+		}
+		
+		[Test]
+		public void Clear_ItemInCollectionAndItemAdded_SetsCountZero ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			rdd.Add ("foobar", "baz");
+			
+			rdd.Clear ();
+			
+			Assert.AreEqual (0, rdd.Count);
+		}
+		
+		[Test]
+		public void Clear_SingleItemAdded_RemovesItem ()
+		{
+			var rdd = new RequestDataDictionary ();
+
+			rdd.Add ("foobar", "baz");
+			rdd.Clear ();
+			
+			string dummy;
+			bool has_item = rdd.ContainsKey ("foobar");
+			
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Clear_ItemInCollection_RemovesItem ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			rdd.Clear ();
+			
+			bool has_item = rdd.ContainsKey ("foobar");
+			
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Clear_ItemInCollectionAndItemAdded_RemovesItem ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			rdd.Add ("foobar", "baz");
+			
+			rdd.Clear ();
+			
+			bool has_item = rdd.ContainsKey ("foobar");
+			
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void ContainsKey_NoItems_ReturnsFalse ()
+		{
+			var rdd = new RequestDataDictionary ();
+
+			bool has_item = rdd.ContainsKey ("foobar");
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void ContainsKey_ItemAdded_ReturnsTrue ()
+		{
+			var rdd = new RequestDataDictionary ();
+			
+			rdd.Add ("foobar", "baz");
+			
+			bool has_item = rdd.ContainsKey ("foobar");
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void ContainsKey_ItemInCollection_ReturnsTrue ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.ContainsKey ("foobar");
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_NoItemsAdded_ReturnsFalse ()
+		{
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string, string> ("foobar", "baz");
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Contains_ItemAddedKeyMatchesValueDoesnt_ReturnsFalse ()
+		{
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string, string> ("foobar", "baz");
+			
+			rdd.Add ("foobar", "nope");
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Contains_ItemAddedKeyDoesNotMatchValueDoes_ReturnsFalse ()
+		{
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string, string> ("foobar", "baz");
+			
+			rdd.Add ("nope", "baz");
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Contains_ItemAddedKeyAndValueMatch_ReturnsTrue ()
+		{
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string, string> ("foobar", "baz");
+			
+			rdd.Add ("foobar", "baz");
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_ItemInCollectionKeyAndValuesMatch_ReturnsTrue ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_MultipleItemsInCollectionFirstKeyAndValuesMatch_ReturnsTrue ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "baz");
+			nvc.Add ("foobar", "blah");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_MultipleItemsInCollectionLastKeyAndValuesMatch_ReturnsTrue ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "blah");
+			nvc.Add ("foobar", "baz");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_MultipleItemsInCollectionMiddleKeyAndValuesMatch_ReturnsTrue ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "blah");
+			nvc.Add ("foobar", "baz");
+			nvc.Add ("foobar", "burrah");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsTrue (has_item);
+		}
+		
+		[Test]
+		public void Contains_MultipleItemsWithPartialStartMatchesInCollection_ReturnsFalse ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "baza");
+			nvc.Add ("foobar", "baza");
+			nvc.Add ("foobar", "baza");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsFalse (has_item);
+		}
+		
+		[Test]
+		public void Contains_MultipleItemsWithPartialEndMatchesInCollection_ReturnsFalse ()
+		{
+			var nvc = new NameValueCollection ();
+			var rdd = new RequestDataDictionary ();
+			var pair = new KeyValuePair<string,string> ("foobar", "baz");
+			
+			nvc.Add ("foobar", "abaz");
+			nvc.Add ("foobar", "abaz");
+			nvc.Add ("foobar", "abaz");
+			rdd.AddCollection (nvc);
+			
+			bool has_item = rdd.Contains (pair);
+			Assert.IsFalse (has_item);
+		}
+	}
+}
+
---
+++ src/Manos/Manos.Server/RequestDataDictionary.cs
@@ -0,0 +1,173 @@
+
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+
+
+namespace Manos.Server {
+
+	public class RequestDataDictionary : IDictionary<string,string> {
+
+		IDictionary<string,string> dictionary = new Dictionary<string,string> ();
+		List<NameValueCollection> data_collections = new List<NameValueCollection> ();
+		
+		public int Count {
+			get {
+				int count = data_collections.Sum (c => c.Count);
+				
+				return count + dictionary.Count;
+			}
+		}
+		
+		public bool IsReadOnly {
+			get { return false; }
+		}
+		
+		public void AddCollection (NameValueCollection col)
+		{
+			data_collections.Add (col);
+		}
+		
+		public void Add (KeyValuePair<string,string> item)
+		{
+			dictionary.Add (item);	
+		}
+		
+		public void Add (string key, string value)
+		{
+			dictionary.Add (key, value);
+		}
+
+		public void Clear ()
+		{
+			dictionary.Clear ();
+			data_collections.ForEach (c => c.Clear ());
+		}
+		
+		public bool Contains (KeyValuePair<string,string> item)
+		{
+			if (dictionary.Contains (item))
+				return true;
+			
+			foreach (NameValueCollection nvc in data_collections) {
+				string value = nvc.Get (item.Key);
+				if (value == null)
+					continue;
+				Console.WriteLine ("VALUE:  '{0}'", value);
+				if (ItemStringContainsValue (value, item.Value))
+				    return true;
+			}
+			
+			return false;
+		}
+		
+		private bool ItemStringContainsValue (string item, string value)
+		{
+			if (item == value)
+				return true;
+			
+			
+			int start = 0;
+			int idx = item.IndexOf (value);
+			
+			while (idx >= 0) {
+				if (idx != 0 && item [idx - 1] != ',') {
+					start = idx + 1;
+					idx = item.IndexOf (value, start);
+					continue;
+				}
+				
+				int end = idx + value.Length;
+				if (end > item.Length)
+					return false;
+				
+				if (end == item.Length || item [end] == ',')
+					return true;
+				
+				start = idx + 1;
+				idx = item.IndexOf (value, start);
+			}
+			
+			return false;
+		}
+		
+		public bool ContainsKey (string key)
+		{
+			if (dictionary.ContainsKey (key))
+				return true;
+
+			return data_collections.Where (c => c.Get (key) != null).FirstOrDefault () != null;
+		}
+
+		public bool Remove (string key)
+		{
+			bool res = false;
+			
+			bool r = dictionary.Remove (key);
+			if (r)
+				res = r;
+			
+			foreach (NameValueCollection nvc in data_collections) {
+				r = nvc.Get (key) != null;
+				nvc.Remove (key);
+				
+				if (r)
+					res = true;
+			}
+			
+			return res;
+		}
+
+		public bool Remove (KeyValuePair<string,string> item)
+		{
+			throw new NotImplementedException ();	
+		}
+		
+		public bool TryGetValue (string key, out string value)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public string this[string key] {
+			get {
+				throw new NotImplementedException ();
+			}
+			set {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public ICollection<string> Keys {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+
+		public ICollection<string> Values {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+		
+		IEnumerator IEnumerable.GetEnumerator ()
+		{
+			throw new NotImplementedException ();
+		}
+		
+		public IEnumerator<KeyValuePair<string,string>> GetEnumerator ()
+		{
+			throw new NotImplementedException ();	
+		}
+		
+		public void CopyTo (KeyValuePair<string,string> [] array, int index)
+		{
+			throw new NotImplementedException ();
+		}
+		
+	}
+}
+
+
