1fabeae8d2643464dbd31a3406d26893ddfa1ef6
Jackson Harper <jacksonh@gmail.com>
2010-10-06 17:55:09 -0400

    Add the writer to the interface.

--- src/Manos/Manos.Server/IHttpResponse.cs
+++ src/Manos/Manos.Server/IHttpResponse.cs
@@ -1,6 +1,7 @@
 
 
 using System;
+using System.IO;
 using System.Text;
 
 namespace Manos.Server {
@@ -16,6 +17,11 @@ namespace Manos.Server {
 		}
 
 		HttpResponseStream Stream {
+			get;
+		}
+
+		
+		StreamWriter Writer {
 			get;
 		}
 
