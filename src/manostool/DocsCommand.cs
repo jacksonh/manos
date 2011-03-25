f71759d5e91e15ea3712edfd8bafe7dac1402903
Jackson Harper <jacksonh@gmail.com>
2011-03-24 23:47:04 -0400

    The two phase startup stuff regressed this. Maybe it would be best for Start to call StartInternal.

--- src/manostool/DocsCommand.cs
+++ src/manostool/DocsCommand.cs
@@ -67,7 +67,8 @@ namespace Manos.Tool
 		{
 			DocsModule docs = new DocsModule (Environment.DocsDirectory);
 			Console.WriteLine ("Go to http://localhost:{0}/ to see your docs.", Port);
-			
+
+			docs.StartInternal ();
 			AppHost.Port = Port;
 			AppHost.Start (docs);
 		}
