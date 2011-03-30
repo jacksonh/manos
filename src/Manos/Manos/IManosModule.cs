8c4a900c3a842c3c2be15c1b375b53a645172207
Antony Pinchbeck <antony@componentx.co.uk>
2011-03-30 12:31:20 +0100

    Added match type to module route setup

--- src/Manos/Manos/IManosModule.cs
+++ src/Manos/Manos/IManosModule.cs
@@ -40,41 +40,49 @@ namespace Manos
 		
 		RouteHandler Route (string pattern, IManosModule module);
 		RouteHandler Route (string pattern, ManosAction action);
+		RouteHandler Route (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Route (ManosAction action, params string [] patterns);
 		RouteHandler Route (IManosModule module, params string [] patterns);
 		
 		RouteHandler Get (string pattern, IManosModule module);
 		RouteHandler Get (string pattern, ManosAction action);
+		RouteHandler Get (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Get (ManosAction action, params string [] patterns);
 		RouteHandler Get (IManosModule module, params string [] patterns);
 		
 	 	RouteHandler Put (string pattern, IManosModule module);
 		RouteHandler Put (string pattern, ManosAction action);
+		RouteHandler Put (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Put (ManosAction action, params string [] patterns);
 		RouteHandler Put (IManosModule module, params string [] patterns);
 		
 		RouteHandler Post (string pattern, IManosModule module);
 		RouteHandler Post (string pattern, ManosAction action);
+		RouteHandler Post (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Post (ManosAction action, params string [] patterns);
 		RouteHandler Post (IManosModule module, params string [] patterns);
 		
 		RouteHandler Delete (string pattern, IManosModule module);
 		RouteHandler Delete (string pattern, ManosAction action);
+		RouteHandler Delete (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Delete (ManosAction action, params string [] patterns);
 		RouteHandler Delete (IManosModule module, params string [] patterns);
 		
 		RouteHandler Head (string pattern, IManosModule module);
 		RouteHandler Head (string pattern, ManosAction action);
+		RouteHandler Head (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Head (ManosAction action, params string [] patterns);
 		RouteHandler Head (IManosModule module, params string [] patterns);
 		
 		RouteHandler Options (string pattern, IManosModule module);
 		RouteHandler Options (string pattern, ManosAction action);
+		RouteHandler Options (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Options (ManosAction action, params string [] patterns);
 		RouteHandler Options (IManosModule module, params string [] patterns);
 		
 		RouteHandler Trace (string pattern, IManosModule module);
 		RouteHandler Trace (string pattern, ManosAction action);
+		RouteHandler Trace (string pattern, ManosAction action, Manos.Routing.MatchType matchType);
 		RouteHandler Trace (ManosAction action, params string [] patterns);
 		RouteHandler Trace (IManosModule module, params string [] patterns);
 
