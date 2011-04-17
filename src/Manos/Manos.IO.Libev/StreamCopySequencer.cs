d568ff3e519dacfa43171c66cdd30570a1f7b639
myeisha <myeisha@quasiparticle.net>
2011-04-17 19:58:02 +0200

    stream copy class with the new streams interface

---
+++ src/Manos/Manos.IO.Libev/StreamCopySequencer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections.Generic;
+using Manos.Collections;
+
+namespace Manos.IO.Libev
+{
+	class StreamCopySequencer : IEnumerable<ByteBuffer>
+	{
+		Stream source, target;
+		ByteBuffer currentBuffer;
+		bool active;
+
+		public StreamCopySequencer (Stream source, Stream target)
+		{
+			this.source = source;
+			this.target = target;
+		}
+
+		IEnumerable<ByteBuffer> CopySequencer ()
+		{
+			active = true;
+			source.Read (OnSourceData, OnSourceError, OnSourceClose);
+			target.PauseWriting ();
+			yield return new ByteBuffer (new byte[0], 0, 0);
+			while (active) {
+				var buffer = currentBuffer;
+				target.PauseWriting ();
+				source.ResumeReading ();
+				yield return buffer;
+			}
+		}
+
+		void OnSourceData (ByteBuffer buffer)
+		{
+			currentBuffer = buffer;
+			target.ResumeWriting ();
+			source.PauseReading ();
+		}
+
+		void OnSourceClose ()
+		{
+			active = false;
+		}
+
+		void OnSourceError (Exception error)
+		{
+			active = false;
+		}
+
+		public IEnumerator<ByteBuffer> GetEnumerator ()
+		{
+			return CopySequencer ().GetEnumerator ();
+		}
+
+		System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator ()
+		{
+			return GetEnumerator ();
+		}
+	}
+}
+
