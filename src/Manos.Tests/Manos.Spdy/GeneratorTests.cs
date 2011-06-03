342898c165410d8e32cc1e91e43d3a0751f4efee
Kerry Snyder <kerry.snyder@gmail.com>
2011-06-03 11:48:19 -0400

    Headers Frame Generation Test

--- src/Manos.Tests/Manos.Spdy/GeneratorTests.cs
+++ src/Manos.Tests/Manos.Spdy/GeneratorTests.cs
@@ -143,6 +143,33 @@ namespace Manos.Spdy.Tests
 			Assert.AreEqual(0x07, fromclass[11], "Last Good Stream ID"); // 8, 9, 10, 11
 			Assert.AreEqual(0x00, fromclass[15], "Status Code"); //12, 13, 14, 15
 		}
+		[Test]
+		public void GenerateHeaders()
+		{
+			HeadersFrame frame = new HeadersFrame();
+			frame.Version = 2;
+			frame.Flags = 0x00;
+			frame.StreamID = 27;
+			NameValueHeaderBlock headers = new NameValueHeaderBlock();
+			headers.Add("Header1", "Value1");
+			headers.Add("Header2", "Value2");
+			frame.Headers = headers;
+			byte[] fromclass = frame.Serialize();
+			Assert.AreEqual(fromclass.Length, frame.Length + 8, "Lengths"); //8 is for control frame header
+			Assert.AreEqual(0x80, fromclass[0], "Control Bit");
+			Assert.AreEqual(0x02, fromclass[1], "Version");
+			Assert.AreEqual(0x08, fromclass[3], "Frame Type"); //skip 2 because type is two bits
+			Assert.AreEqual(0x00, fromclass[4], "Flags");
+			// 5,6,7 are Length, Will add test once I actually implement packet generation
+			Assert.AreEqual(0x1B, fromclass[11], "Stream ID"); //8, 9, 10, 11 -> Stream ID
+			byte[] output = new byte[0];
+			int declength = Compression.Inflate(fromclass, 12 , frame.Length - 4, out output);
+			byte[] nv = headers.UncompressedSerialize();
+			for (int i = 0; i < declength; i++)
+			{
+				Assert.AreEqual(nv[i], output[i], "Name Value Block Byte #" + i);
+			}
+		}
 	}
 }
 
