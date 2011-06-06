1ea6b56db175d1d5de3f5ad3b9952efe0354f0fa
Kerry Snyder <kerry.snyder@gmail.com>
2011-06-06 17:45:57 -0400

    Syn Reply Serialization finished and passing

--- src/Manos/Manos.Spdy/SynReplyFrame.cs
+++ src/Manos/Manos.Spdy/SynReplyFrame.cs
@@ -8,6 +8,7 @@ namespace Manos.Spdy
 		public NameValueHeaderBlock Headers { get; set; }
 		public SynReplyFrame ()
 		{
+			this.Type = ControlFrameType.SYN_REPLY;
 		}
 		public SynReplyFrame(byte[] data, int offset, int length)
 		{
@@ -15,6 +16,15 @@ namespace Manos.Spdy
 			base.Parse(data, offset, length);
 			this.StreamID = Util.BuildInt(data, offset + 8, 4);
 			this.Headers = NameValueHeaderBlock.Parse(data, offset + 12, length - 12);
+		}
+		public new byte[] Serialize()
+		{
+			byte[] nvblock = this.Headers.Serialize();
+			this.Length = nvblock.Length + 4;
+			var header = base.Serialize();
+			byte[] middle = new byte[4];
+			Util.IntToBytes(this.StreamID, ref middle, 0, 4);
+			return Util.Combine(header, middle, nvblock);
 		}
 	}
 }
