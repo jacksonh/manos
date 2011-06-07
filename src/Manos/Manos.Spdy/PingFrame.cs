bee12ca2edbaf30a2eaa4170526d190f19b83fee
Kerry Snyder <kerry.snyder@gmail.com>
2011-06-06 21:10:02 -0400

    Ping frame serialization implemented

--- src/Manos/Manos.Spdy/PingFrame.cs
+++ src/Manos/Manos.Spdy/PingFrame.cs
@@ -7,12 +7,21 @@ namespace Manos.Spdy
 		public int ID { get; set; }
 		public PingFrame()
 		{
+			this.Type = ControlFrameType.PING;
 		}
 		public PingFrame(byte[] data, int offset, int length)
 		{
 			this.Type = ControlFrameType.PING;
 			base.Parse(data, offset, length);
 			this.ID = Util.BuildInt(data, offset + 8, 4);
+		}
+		public new byte[] Serialize()
+		{
+			this.Length = 4;
+			var headers = base.Serialize();
+			Array.Resize(ref headers, 12);
+			Util.IntToBytes(this.ID, ref headers, 8, 4);
+			return headers;
 		}
 	}
 }
