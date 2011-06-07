ab63e45b786bf65a12e81771d6a3b0010bfa1ef4
Kerry Snyder <kerry.snyder@gmail.com>
2011-06-06 21:18:53 -0400

    Window Update Frame Serialization

--- src/Manos/Manos.Spdy/WindowUpdateFrame.cs
+++ src/Manos/Manos.Spdy/WindowUpdateFrame.cs
@@ -8,6 +8,7 @@ namespace Manos.Spdy
 		public int DeltaWindowSize { get; set; }
 		public WindowUpdateFrame ()
 		{
+			this.Type = ControlFrameType.WINDOW_UPDATE;
 		}
 		public WindowUpdateFrame(byte[] data, int offset, int length)
 		{
@@ -15,6 +16,15 @@ namespace Manos.Spdy
 			base.Parse(data, offset, length);
 			this.StreamID = Util.BuildInt(data, offset + 8, 4);
 			this.DeltaWindowSize = Util.BuildInt(data, offset + 12, 4);
+		}
+		public new byte[] Serialize()
+		{
+			this.Length = 8;
+			var headers = base.Serialize();
+			Array.Resize(ref headers, 16);
+			Util.IntToBytes(this.StreamID, ref headers, 8, 4);
+			Util.IntToBytes(this.DeltaWindowSize, ref headers, 12, 4);
+			return headers;
 		}
 	}
 }
