ca4b8d041c40b7a0a2f51a6c11e1903272dd4270
Kerry Snyder <kerry.snyder@gmail.com>
2011-06-08 20:44:18 -0400

    Change Compression to use instances to get the stream context thing right

--- src/Manos/Manos.Spdy/Compression.cs
+++ src/Manos/Manos.Spdy/Compression.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace Manos.Spdy
 {
-	public static class Compression
+	public abstract class ZlibContext
 	{
-		private static string dict = "optionsgetheadpostputdeletetraceacceptaccept-charsetaccept-encodingaccept-" +
+		internal static string dict = "optionsgetheadpostputdeletetraceacceptaccept-charsetaccept-encodingaccept-" +
 							 "languageauthorizationexpectfromhostif-modified-sinceif-matchif-none-matchi" +
 						  	 "f-rangeif-unmodifiedsincemax-forwardsproxy-authorizationrangerefererteuser" +
 							 "-agent10010120020120220320420520630030130230330430530630740040140240340440" +
@@ -18,64 +18,67 @@ namespace Manos.Spdy
 							 "ndayTuesdayWednesdayThursdayFridaySaturdaySundayJanFebMarAprMayJunJulAugSe" +
 							 "pOctNovDecchunkedtext/htmlimage/pngimage/jpgimage/gifapplication/xmlapplic" +
 							 "ation/xhtmltext/plainpublicmax-agecharset=iso-8859-1utf-8gzipdeflateHTTP/1" +
-							 ".1statusversionurl"; // + char.MinValue;
-		public static int Inflate(byte[] input, int offset, int length, out byte[] output)
+							 ".1statusversionurl"+ char.MinValue;
+		internal ZlibCodec stream;
+		internal int bytes;
+		public ZlibContext()
 		{
-			int error;
+			stream = new ZlibCodec();
+		}
+	}
+	public class InflatingZlibContext : ZlibContext
+	{
+		public InflatingZlibContext() : base()
+		{
+			this.stream.InitializeInflate();
+		}
+		public int Inflate(byte[] input, int offset, int length, out byte[] output)
+		{
 			output = new byte[40000];
-			ZlibCodec stream = new ZlibCodec();
 			stream.InputBuffer = input;
 			stream.NextIn = offset;
 			stream.AvailableBytesIn = length;
-			error = stream.InitializeInflate();
-			CheckForError(stream, error, "Init");
 			stream.OutputBuffer = output;
 			stream.NextOut = 0;
 			stream.AvailableBytesOut = output.Length;
-			error = stream.Inflate(FlushType.Sync);
+			int error = stream.Inflate(FlushType.Sync);
 			if (error == ZlibConstants.Z_NEED_DICT) {
-				error = stream.SetDictionary(Encoding.ASCII.GetBytes(dict));
+				stream.SetDictionary(Encoding.UTF8.GetBytes(dict));
+				stream.Inflate(FlushType.Sync);
 			}
-			error = stream.Inflate(FlushType.Sync);
-			CheckForError(stream, error, "inflate");
-			error = stream.EndInflate();
-			CheckForError(stream, error, "End");
-			return (int)stream.TotalBytesOut;
-		}
-		public static int Deflate(byte[] input, int offset, int length, out byte[] output)
-		{
-			int error;
-			output = new byte[40000];
-			ZlibCodec stream = new ZlibCodec();
-			stream.InputBuffer = input;
-			stream.NextIn = offset;
-			stream.AvailableBytesIn = length;
-			error = stream.InitializeDeflate();
-			CheckForError(stream, error, "Init");
-			stream.OutputBuffer = output;
-			stream.NextOut = 0;
-			stream.AvailableBytesOut = output.Length;
-			error = stream.Deflate(FlushType.Sync);
-			if (error == ZlibConstants.Z_NEED_DICT) {
-				error = stream.SetDictionary(Encoding.ASCII.GetBytes(dict));
+			int ret = (int)stream.TotalBytesOut - bytes;
+			bytes = (int)stream.TotalBytesOut;
+			if (bytes > 4000) {
+				output = new byte[4000];
 			}
-			error = stream.Deflate(FlushType.Sync);
-			CheckForError(stream, error, "inflate");
-			error = stream.EndDeflate();
-			CheckForError(stream, error, "End");
-			return (int)stream.TotalBytesOut;
+			return ret;	
 		}
-		internal static void CheckForError(ZlibCodec z, int err, System.String msg)
-    	{
-        	if (err != ZlibConstants.Z_OK)
-        	{
-            	//if (z.Message != null)
-            	    //System.Console.Out.Write(z.Message + " ");
-            	//System.Console.Out.WriteLine(msg + " error: " + err);
 
-            	//System.Environment.Exit(1);
-        	}
-    	}
+	}
+	public class DeflatingZlibContext : ZlibContext
+	{
+		public DeflatingZlibContext() : base()
+		{
+			this.stream.InitializeDeflate();
+		}
+		public int Deflate(byte[] input, int offset, int length, out byte[] output)
+		{
+			output = new byte[40000];
+			stream.InputBuffer = input;
+			stream.NextIn = offset;
+			stream.AvailableBytesIn = length;
+			stream.OutputBuffer = output;
+			stream.NextOut = 0;
+			stream.AvailableBytesOut = output.Length;
+			int error = stream.Deflate(FlushType.Sync);
+			if (error == ZlibConstants.Z_NEED_DICT) {
+				stream.SetDictionary(Encoding.UTF8.GetBytes(dict));
+				stream.Deflate(FlushType.Sync);
+			}
+			int ret = (int)stream.TotalBytesOut - bytes;
+			bytes = (int)stream.TotalBytesOut;
+			return ret;	
+		}
 	}
 }
 
